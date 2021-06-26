411848ce821eb402337d27fb4aab37878188160c
Lucas Lima <lucas_sdl@hotmail.com>
2021-06-26 16:39:06 -0300

    05. Inbound properties: passando dados para o componente

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
   templateUrl: 'photo.component.html'
 })
 export class PhotoComponent{
-  description = 'New York';
-  url = "https://i.guim.co.uk/img/media/7cef2d0585a6aed1638f43b4fc10fb0945b39f3f/0_0_6720_4480/master/6720.jpg?width=1020&quality=85&auto=format&fit=max&s=702214986431d2c40e87dc4f34844cfd";
+  @Input() description = '';
+  @Input() url = "";
 }
