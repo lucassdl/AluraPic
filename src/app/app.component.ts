e07d83597469b6aa86727c162c1fd76842ca029a
Lucas Lima <lucas_sdl@hotmail.com>
2021-06-27 08:33:00 -0300

    09. Utilizando a primeira diretiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  photos = [
+    {
+      url: 'https://i.guim.co.uk/img/media/7cef2d0585a6aed1638f43b4fc10fb0945b39f3f/0_0_6720_4480/master/6720.jpg?width=1020&quality=85&auto=format&fit=max&s=702214986431d2c40e87dc4f34844cfd',
+      description: 'New Iork'
+    },
+    {
+      url: 'https://ogimg.infoglobo.com.br/in/13895040-e78-79c/FT1086A/xquebec.jpg.pagespeed.ic.ve6QpxzeUx.jpg',
+      description: 'Quebec'
+    }
+  ]
 }
