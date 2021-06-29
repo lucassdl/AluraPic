44994b780822b6e3d6612d9610c886f227469d69
Lucas Lima <lucas_sdl@hotmail.com>
2021-06-29 18:45:25 -0300

    05. HttpClient e injeção de dependência

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,18 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [
-    {
-      url: 'https://i.guim.co.uk/img/media/7cef2d0585a6aed1638f43b4fc10fb0945b39f3f/0_0_6720_4480/master/6720.jpg?width=1020&quality=85&auto=format&fit=max&s=702214986431d2c40e87dc4f34844cfd',
-      description: 'New Iork'
-    },
-    {
-      url: 'https://ogimg.infoglobo.com.br/in/13895040-e78-79c/FT1086A/xquebec.jpg.pagespeed.ic.ve6QpxzeUx.jpg',
-      description: 'Quebec'
-    },
-    {
-      url: 'https://www.infomoney.com.br/wp-content/uploads/2019/06/lisboa.jpg?fit=900%2C600&quality=50&strip=all',
-      description: 'Lisboa'
-    }
-  ]
+  photos = [];
+
+  constructor(http: HttpClient){
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { PhotosModule } from './photos/photos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
