78155f329cc7842c122ed437996883fd4fe85059
Lucas Lima <lucas_sdl@hotmail.com>
2021-08-21 15:10:57 -0300

    05. Resolvers

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,3 +1,4 @@
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -6,14 +7,26 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
-    { path: 'user/:userName', component: PhotoListComponent },
-    { path: 'p/add', component: PhotoFormComponent },
-    { path: '**', component: NotFoundComponent }  
+    {
+      path: 'user/:userName',
+      component: PhotoListComponent,
+      resolve: {
+        photos: PhotoListResolver
+      }
+    },
+    {
+      path: 'p/add',
+      component: PhotoFormComponent
+    },
+    {
+      path: '**',
+      component: NotFoundComponent
+    }
 ];
 
 @NgModule({
-    imports: [ 
-        RouterModule.forRoot(routes) 
+    imports: [
+        RouterModule.forRoot(routes)
     ],
     exports: [ RouterModule ]
 })
